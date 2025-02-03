8efb1dad7b3164ecaf890dff034ad9e1dc024f14
Om Patil <159125383+omsandeeppatil-pvt@users.noreply.github.com>
2025-02-03 16:44:20 +0000

    Updated Reddit Component

--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,9 @@ interface ResultsSectionProps {
 // Component configuration
 const SEARCH_COMPONENTS: SearchComponentConfig[] = [
   // { name: 'ImageResult', component: lazy(() => import("app/components/ui/ImageResult")) },
-   { name: 'GoogleAnalytics', component: lazy(() => import("app/components/ui/GoogleAnalytics")) }
+  // { name: 'GoogleAnalytics', component: lazy(() => import("app/components/ui/GoogleAnalytics")) }
   // { name: 'PlayStoreAnalytics', component: lazy(() => import("app/components/ui/PlayStoreAnalytics")) },
-  // { name: 'RedditAnalytics', component: lazy(() => import("app/components/ui/RedditAnalytics")) },
+   { name: 'RedditAnalytics', component: lazy(() => import("app/components/ui/RedditAnalytics")) },
   // { name: 'YoutubeAnalysis', component: lazy(() => import("app/components/ui/YoutubeAnalysis")) },
   // { name: 'QuoraAnalysis', component: lazy(() => import("app/components/ui/QuoraAnalysis")) }
   // { name: 'TrendAnalysis', component: lazy(() => import("app/components/ui/TrendAnalysis")) },
