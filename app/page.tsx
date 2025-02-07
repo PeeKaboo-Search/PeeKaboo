5c3b29124721bcc460479fcb7861e29f07d47316
Om Patil <159125383+omsandeeppatil-pvt@users.noreply.github.com>
2025-02-07 08:08:31 +0000

    Facebook try

--- app/page.tsx
+++ app/page.tsx
@@ -26,17 +26,18 @@ interface ResultsSectionProps {
 // Component configuration
 const SEARCH_COMPONENTS: SearchComponentConfig[] = [
   // { name: 'ImageResult', component: lazy(() => import("app/components/ui/ImageResult")) },
-   { name: 'GoogleAnalytics', component: lazy(() => import("app/components/ui/GoogleAnalytics")) },
+  // { name: 'GoogleAnalytics', component: lazy(() => import("app/components/ui/GoogleAnalytics")) },
   // { name: 'PlayStoreAnalytics', component: lazy(() => import("app/components/ui/PlayStoreAnalytics")) },
   // { name: 'RedditAnalytics', component: lazy(() => import("app/components/ui/RedditAnalytics")) },
   // { name: 'YoutubeAnalysis', component: lazy(() => import("app/components/ui/YoutubeAnalysis")) },
   // { name: 'QuoraAnalysis', component: lazy(() => import("app/components/ui/QuoraAnalysis")) }
+  { name: 'FacebookAdsAnalysis', component: lazy(() => import("app/components/ui/FacebookAdsAnalytics")) },
   // { name: 'TrendAnalysis', component: lazy(() => import("app/components/ui/TrendAnalysis")) },
   // { name: 'RunningAds', component: lazy(() => import("app/components/ui/RunningAds")) }
   // { name: 'AdsAnalytics', component: lazy(() => import("app/components/ui/AdsAnalytics")) },
   // { name: 'NewsResults', component: lazy(() => import("app/components/ui/NewsResults")) },
   // { name: 'TopPainpoints', component: lazy(() => import("app/components/ui/TopPainpoints")) },
-   { name: 'TopTriggers', component: lazy(() => import("app/components/ui/TopTriggers")) },
+  // { name: 'TopTriggers', component: lazy(() => import("app/components/ui/TopTriggers")) },
   // { name: 'Recommended', component: lazy(() => import("app/components/ui/Recommended")) },
   // { name: 'StrategyAnalysis', component: lazy(() => import("app/components/ui/StrategyAnalysis")) },
   // { name: 'SentimentAnalysis', component: lazy(() => import("app/components/ui/SentimentAnalysis")) },
