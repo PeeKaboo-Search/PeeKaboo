a16d86a17fb0931ff077d063493c7c05639b8fc0
Om  Patil <omsandeeppatil02@gmail.com>
2025-06-06 11:46:09 +0000

    Fixed Deployment issues

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { getAllApiModels, getAvailableGroqModels, updateModelName, ApiModel, GroqModel } from '../api/main/models';
 
